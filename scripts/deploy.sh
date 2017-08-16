7e868af76d1661a3a0f91437bcd4c906cfb2f9fc
Lev Lazinskiy <lev@levlaz.org>
2017-08-16 05:11:09 +0000

    dlc

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@
 
 scp docker-compose.yml ubuntu@34.211.187.29:
 
-ssh ubuntu@34.211.187.29 'docker-compose pull'
-ssh ubuntu@34.211.187.29 'docker-compose build'
-ssh ubuntu@34.211.187.29 'docker-compose up -d'
+ssh ubuntu@34.211.187.29 'sudo docker-compose pull'
+ssh ubuntu@34.211.187.29 'sudo docker-compose build'
+ssh ubuntu@34.211.187.29 'sudo docker-compose up -d'
